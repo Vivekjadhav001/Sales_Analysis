af1bc6b6d12bcd5c0891252e61c3acd95ae19588
Vivekjadhav001 <167954880+Vivekjadhav001@users.noreply.github.com>
2024-05-08 10:52:49 +0530

    Update Sales Analysis.py

--- Sales Analysis.py
+++ Sales Analysis.py
@@ -1,30 +1,26 @@
 import pandas as pd
-# file="G:/Datasets/Sales_Data/Sales_April_2019.csv"
-all_data=pd.read_csv("final output.csv")
-# print(all_data)
 
 ##[TASK 1]
 ### Merging all month sales data into one file(CSV)
-# import os
-# files = [file for file in os.listdir("G:/Datasets/Sales_Data")]
+ import os
+ files = [file for file in os.listdir("G:/Datasets/Sales_Data")]
 
-# all_month_data=pd.DataFrame()
+all_month_data=pd.DataFrame()
+for file in files:
+     df=pd.read_csv("G:/Datasets/Sales_Data/"+file)
+     all_month_data=pd.concat([all_month_data,df])
 
-# for file in files:
-#     df=pd.read_csv("G:/Datasets/Sales_Data/"+file)
-#     all_month_data=pd.concat([all_month_data,df])
+ all_month_data.to_csv("final output.csv",index=False)
 
-# all_month_data.to_csv("final output.csv",index=False)
-
-# all_data=pd.read_csv("final output.csv")
-# print(all_data.head())
+ all_data=pd.read_csv("final output.csv")
+ print(all_data.head())
 
 ##[TASK 2]
 ##Deleteing missing values rows
 
 all_data=all_data.dropna()
 print("Empty  rows deleted succesfully")
-#print(all_data)
+print(all_data)
 
 ##[TASK 3]
 ##Finding an error of "Or" 
@@ -49,18 +45,18 @@ print("Sales Calculated successfully")
 print(all_data.head(10))
 
 ##Question 1:What was the best month for sales?How much we earned in that month?
-# results=all_data.groupby('Month').sum()
-# print(results)
+ results=all_data.groupby('Month').sum()
+ print(results)
 
 ##Ploting a graph for Question 1
 import matplotlib.pyplot as plt
-# month=range(1,13)
-# plt.bar(month,results['Total Sales'])
-# plt.title("Best Month For Sale")
-# plt.xticks(month)
-# plt.xlabel("Months")
-# plt.ylabel("Sales")
-# plt.show()
+ month=range(1,13)
+ plt.bar(month,results['Total Sales'])
+ plt.title("Best Month For Sale")
+ plt.xticks(month)
+ plt.xlabel("Months")
+ plt.ylabel("Sales")
+ plt.show()
 
 ##Additional Funtion to get city from adress
 def get_city(adress):
@@ -76,13 +72,13 @@ print(all_data.head())
 # print(high_city)
 
 ##Ploting a graph for Question 2
-# city=[city for city,df in all_data.groupby('City')]
-# plt.bar(city,high_city["Total Sales"])
-# plt.title("Highest City in Sales")
-# plt.xticks(city,rotation='vertical',size=8)
-# plt.xlabel("City")
-# plt.ylabel("Sales")
-# plt.show()
+city=[city for city,df in all_data.groupby('City')]
+plt.bar(city,high_city["Total Sales"])
+plt.title("Highest City in Sales")
+plt.xticks(city,rotation='vertical',size=8)
+plt.xlabel("City")
+plt.ylabel("Sales")
+plt.show()
 
 ##[TASK 7]
 ##Question No 3:What time is good to show advtisment ? 
@@ -93,38 +89,38 @@ all_data["Minutes"]=all_data["Order Date"].dt.minute
 print(all_data.head())
 
 ##Ploting a graph for Question 3
-# hours=[hours for hours,df in all_data.groupby('Hours')]
-# plt.plot(hours,all_data.groupby("Hours").count())
-# plt.title("GOOD TIME TO DISPLAY ADVITISMENT")
-# plt.xticks(hours)
-# plt.xlabel("Hours")
-# plt.ylabel("Viewers Count")
-# plt.grid()
-# plt.show()
+hours=[hours for hours,df in all_data.groupby('Hours')]
+plt.plot(hours,all_data.groupby("Hours").count())
+plt.title("GOOD TIME TO DISPLAY ADVITISMENT")
+plt.xticks(hours)
+plt.xlabel("Hours")
+plt.ylabel("Viewers Count")
+plt.grid()
+plt.show()
 
 ##Question No 4:What products are often sold togeather?
 ##Grouping all same order id and  joining the products baught
 df=all_data[all_data["Order ID"].duplicated(keep=False)]
 df["Grouped"]=df.groupby("Order ID")["Product"].transform(lambda x:",".join(x))
-# print(df.head(10))
+print(df.head(10))
 
 ##Deleting the duplicates now
 df=df[["Order ID","Grouped"]].drop_duplicates()
-# print(df)
+print(df)
 
 ##Checking the most  common product with its count
 
 from itertools import combinations
 from collections import Counter
 
-# count=Counter()
+count=Counter()
 
-# for row in df["Grouped"]:
-#     row_list=row.split(",")
-#     count.update(combinations(row_list,2))
+for row in df["Grouped"]:
+     row_list=row.split(",")
+    count.update(combinations(row_list,2))
 
-# for key,value in count.most_common(10):
-#     print(f"{key}: {value}")
+ for key,value in count.most_common(10):
+     print(f"{key}: {value}")
 
 
 ##Question 5:Which product sold the most ? why do you think it sold the most?
